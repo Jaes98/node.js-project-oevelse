ea71dddc2c6f2a71c1256704b7255ef3883ad948
Jaes98 <marcus.jaes@gmail.com>
2023-08-24 10:44:03 +0200

    8-11 opgaver done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import fs from "fs/promises";
 import cors from "cors";
+import { log } from "console";
 
 const app = express();
 const port = 3333;
@@ -26,3 +27,56 @@ app.get("/users", async (request, response) => {
     console.log(users);
     response.json(users)
 });
+
+app.post("/users", async (request, response) => {
+    const newUser = request.body;
+    newUser.id = new Date().getTime();
+    console.log(newUser);
+
+    const data = await fs.readFile("data.json");
+    const users = JSON.parse(data)
+
+    users.push(newUser);
+    console.log(newUser);
+
+    fs.writeFile("data.json", JSON.stringify(users));
+
+    response.json(users);
+});
+
+app.put("/users/:id", async (request, response) => {
+    const id = Number(request.params.id);
+    console.log(id);
+
+    const data = await fs.readFile("data.json");
+    const users = JSON.parse(data);
+
+    let userToUpdate = users.find(user => user.id === id)
+
+    const body = request.body;
+    console.log(body);
+    userToUpdate.image = body.image;
+    userToUpdate.mail = body.mail;
+    userToUpdate.name = body.name;
+    userToUpdate.title = body.title;
+
+    fs.writeFile("data.json", JSON.stringify(users));
+    response.json(users);
+
+})
+
+app.delete("/users/:id", async (request, response) => {
+    const id = Number(request.params.id);
+    console.log(id);
+
+    const data = await fs.readFile("data.json");
+    const users = JSON.parse(data);
+
+    const newUsers = users.filter(user => user.id !==id)
+
+    fs.writeFile("data.json", JSON.stringify(newUsers));
+
+    newUsers
+
+    response.json(users);
+})
